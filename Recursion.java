88b0c46beac7a0c645eec2add16b7da5a3eb5410
Ahbab Ashraf <ahbab.ashraf@gmail.com>
2017-02-06 22:46:59 -0500

    fixed algorithm

--- Recursion.java
+++ Recursion.java
@@ -5,20 +5,23 @@ public class Recursion {
     }
 
     public static double sqrt(double num) {
-	return sqrtHelper(num, num);
+	return sqrtHelper(num, 1.0);
     }
 
     private static double sqrtHelper(double target, double num) {
-	double betterNum = (target / num + num) / 2;
-	double percentDif = (num - betterNum) / (num + betterNum) / 2;
-	if (percentDif < 0000000000.1) {
+	double betterNum = (target / num + num) / 2.0;
+	double difference = Math.abs(num - betterNum) / ((num + betterNum) /2);
+	if (difference < 0.00000000000001) {
 	    return num;
 	} else {
+	    
 	    return sqrtHelper(target, betterNum);
 	}
     }
 
     public static void main (String[] args) {
+
+	System.out.println(sqrt(4));
 
 	for (int count = 0; count < 11; count++) {
 	    double randNum = Math.random() * 100000 + 1;

e4f0214ea13f399dc6a8c207e8a4a1e8d2830d56
Ahbab Ashraf <ahbab.ashraf@gmail.com>
2017-02-11 22:57:47 -0500

    Did work on add and remove queen, still needs tweaking

--- 02NQueens/QueenBoard.java
+++ 02NQueens/QueenBoard.java
@@ -9,6 +9,38 @@ public class QueenBoard {
 	updateBoardState();
     }
 
+    private void addQueen (int row, int col) {
+	
+	for (int count = 0; count < board.length; count++) {
+	    board[count][col] += 1;
+	    board[row][count] += 1;
+	}
+	
+	for (int count = row, num = col; count > -1 && num > -1; count--, num--) {
+	    board[count][num] += 1;
+	}
+	for (int count = row, num = col; count < board.length && num < board.length; count++, num++) {
+	    board[count][num] += 1;
+	}
+
+	for (int count = 0, num = 0; count < board.length && num < -1; count++, num--) {
+	    board[count][num] += 1;
+	}
+	for (int count = 0, num = 0; count > -1 && num < board.length; count--, num++) {
+	    board[count][num] += 1;
+	}
+	
+	board[row][col] = -1;
+    }
+
+    private void removeQueen (int row, int col) {
+	for (int count = 0; count < board.length; count++) {
+	    board[count][col] -= 1;
+	    board[row][count] -= 1;
+	}
+	board[row][col] = 0;
+    }
+
     public void solve () {
 
     }
@@ -17,11 +49,15 @@ public class QueenBoard {
 
     }
 
-    private void updateBoardState () {
+    public void updateBoardState () {
 	boardState = "";
 	for (int[] ary : board) {
 	    for (int num : ary) {
-		boardState += num;
+		if (num == -1) {
+		    boardState += "Q ";
+		} else {
+		    boardState += num + " ";
+		}
 	    }
 	    boardState += "\n";
 	}
@@ -33,6 +69,11 @@ public class QueenBoard {
 
     public static void main (String[] args) {
 	QueenBoard board = new QueenBoard(8);
+	board.addQueen(4, 4);
+	board.updateBoardState();
+	System.out.println(board.toString());
+	board.removeQueen(4, 4);
+	board.updateBoardState();
 	System.out.println(board.toString());
     }
 

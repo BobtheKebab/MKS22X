48299e56d65ae6f729ec9ddc103c71bc4acf6749
Ahbab Ashraf <ahbab.ashraf@gmail.com>
2017-03-07 19:43:19 -0500

    Edited class structure to comply with instructions + improved toString to handle 1 digit numbers

--- 05USACO/USACO.java
+++ 05USACO/USACO.java
@@ -3,49 +3,66 @@ import java.io.*;
 
 public class USACO {
 
-    public int R, C, E, N;
-    private int[][] lake;
-    public static int[][] orders;
+    //public int R, C, E, N;
+    //private int[][] lake;
+    //public static int[][] orders;
 
-    public USACO (String fileName) {
+    public USACO () {
+
+    }
+
+    public static int silver (String fileName) {
+
+	int R, C, E, N, answer = 0;
+	int[][] lake, orders;
+
 	try {
+
+	    // Get all info from input
+
 	    File data = new File(fileName);
-	    getInfo(data);
-	} catch (Exception e) {
+	    Scanner skan = new Scanner(data);
+	    R = Integer.parseInt(skan.next());
+	    C = Integer.parseInt(skan.next());
+	    E = Integer.parseInt(skan.next());
+	    N = Integer.parseInt(skan.next());
+	    lake = new int[R][C];
+	    for(int i = 0; i < R; i++){
+		for (int count = 0; count < C; count++) {
+		    lake[i][count] = Integer.parseInt(skan.next());
+		}
+	    }
+	
+	    orders = new int[N][3];
+	    for (int count = 0; count < N; count++) {
+		for (int num = 0; num < 3; num++) {
+		    orders[count][num] = Integer.parseInt(skan.next());
+		}
+	    }
+
+	    System.out.println(toString(lake));
+	    System.out.println(toString(orders));
+
+	    // Run calculations and return output
+
+	} catch (FileNotFoundException e) {
 	    System.out.println(e);
 	    System.exit(0);
 	}
+
+	return answer;
+	
     }
 
-    private void getInfo (File data) throws FileNotFoundException {
-	
-	Scanner skan = new Scanner(data);
-	R = Integer.parseInt(skan.next());
-	C = Integer.parseInt(skan.next());
-	E = Integer.parseInt(skan.next());
-	N = Integer.parseInt(skan.next());
-	lake = new int[R][C];
-	for(int i = 0; i < R; i++){
-	     for (int count = 0; count < C; count++) {
-		lake[i][count] = Integer.parseInt(skan.next());
-	    }
-	}
-	//System.out.println(toString());
-	
-	orders = new int[N][3];
-	for (int count = 0; count < N; count++) {
-	    for (int num = 0; num < 3; num++) {
-		orders[count][num] = Integer.parseInt(skan.next());
-	    }
-	}
-	//System.out.println(USACO.orders[0]);
-    }
-
-    public String toString () {
+    public static String toString (int[][] array) {
 	String str = "";
-	for (int[] ary : lake) {
+	for (int[] ary : array) {
 	    for (int num : ary) {
-		str += num + " ";
+		if (num < 10) {
+		    str += num + "  ";
+		} else {
+		    str += num + " ";
+		}
 	    }
 	    str += "\n";
 	}
@@ -55,9 +72,9 @@ public class USACO {
 
     public static void main (String[] args) {
 	
-	USACO dank = new USACO("input1.txt");
+	USACO dank = new USACO();
 	
-	System.out.println(USACO.orders[1][2]);
+	System.out.println(USACO.silver("input1.txt"));
 
     }
 }

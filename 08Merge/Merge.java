912dfe7d78b9fa2a7b7889c776f54a990cb3c0eb
Ahbab Ashraf <ahbab.ashraf@gmail.com>
2017-03-27 19:11:49 -0400

    Got mergesort to work; giving merge a destination array was the main fix

--- 08Merge/Merge.java
+++ 08Merge/Merge.java
@@ -4,54 +4,58 @@ public class Merge {
 
     // Mandatory Tests: random big, random small, same value, sorted, reverse sorted
 
-    private static int[] merge (int[] ary, int[] yar) {
-	int[] answer = new int[ary.length + yar.length];
+    private static int[] merge (int[] dest, int[] ary, int[] yar) {
+	dest = new int[ary.length + yar.length];
 	int ind1 = 0, ind2 = 0;
 	boolean bool = true;
-	
+	    
 	while (ind1 < ary.length && ind2 < yar.length) {
 
 	    if (ary[ind1] < yar[ind2]) {
-		answer[ind1 + ind2] = ary[ind1];
+		dest[ind1 + ind2] = ary[ind1];
 		ind1++;
 	    } else {
-		answer[ind1 + ind2] = yar[ind2];
+		dest[ind1 + ind2] = yar[ind2];
 		ind2++;
 	    }
-	    if (ind1 == yar.length - 1) bool = false;
-
+	    if (ind1 == ary.length) bool = false;
 	}
 
 	if (bool) {
 	    while (ind1 < ary.length) {
-		answer[ind1 + ind2] = ary[ind1];
+		dest[ind1 + ind2] = ary[ind1];
 		ind1++;
 	    }
 	} else {
 	    while (ind2 < yar.length) {
-		answer[ind1 + ind2] = yar[ind2];
+		dest[ind1 + ind2] = yar[ind2];
 		ind2++;
 	    }
 	}
 
-	return answer;
+	return dest;
     }
 
     private static int[] makeCopy (int[] ary, int start, int end) {
-	int answer = new int[end - start];
-	for (int count = 0; count < end - start; count++, start++) {
+	int[] answer = new int[end - start];
+	for (int count = 0; count < end - start + 1; count++, start++) {
 	    answer[count] = ary[start];
 	}
 	return answer;
     }
 
     public static void mergesort (int[] ary) {
-	int half = (ary.length - 1) / 2;
-	int[] left = makeCopy(ary, half);
-	int[] right = makeCopy(half + 1, ary.length - 1);
+	
+	if (ary.length == 1) {
+	    return;
+	}
+	
+	int half = ary.length / 2;
+	int[] left = makeCopy(ary, 0, half);
+	int[] right = makeCopy(ary, half, ary.length);
 	mergesort(left);
 	mergesort(right);
-	ary = merge(left, right);
+	merge(ary, left, right);
     }
 
     public static String toString (int[] ary) {
@@ -66,9 +70,13 @@ public class Merge {
 
 	Merge dank = new Merge();
 	int[] a = {1, 7};
-	int[] b = {5, 10};
-	System.out.println(merge(a, b)[3]);
-	
+	int[] b = {5, 10, 2, 8};
+	//int[] d = merge(d, a, b);
+	//System.out.println(toString(d));
+
+	int[] c = {5, 11, 6, 9};
+	mergesort(c);
+	System.out.println(toString(c));
     }
 }
 

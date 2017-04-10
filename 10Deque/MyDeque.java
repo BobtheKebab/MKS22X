337219c9d6852cad7f75a053664514fe255c3912
Ahbab Ashraf <ahbab.ashraf@gmail.com>
2017-04-10 10:48:33 -0400

    Wrote class and function shells

---
+++ 10Deque/MyDeque.java
@@ -0,0 +1,40 @@
+public class MyDeque {
+
+    private int size, front, back;
+    private LinkedList<String> list;
+
+    public MyDeque () {
+	size = 5;
+	String[] temp = new String[size];
+	list = new LinkedList<String>(temp);
+    }
+
+    public void addFirst (String str) {
+
+    }
+
+    public void addLast (String str) {
+
+    }
+
+    public String removeFirst () {
+
+    }
+
+    public String removeLast () {
+
+    }
+
+    public String getFirst () {
+	
+    }
+
+    public String getLast () {
+
+    }
+
+    private void doubleSize () {
+
+    }
+
+}

474124959c721ba1f3ab9ded4fc848b250b8b9b5
Ahbab Ashraf <ahbab.ashraf@gmail.com>
2017-04-20 21:48:23 -0400

    Wrote and tested eval

--- Eval/Eval.java
+++ Eval/Eval.java
@@ -1,20 +1,36 @@
+import java.util.ArrayDeque;
+
 public class Eval {
 
-    private double apply (String op, double num1, double num2) {
-
+    static String operations = "+-*/%";
+    
+    private static double apply (String op, double a, double b) {
+        double[] ary = { b + a, b - a, b * a, b / a, b % a };
+	return ary[operations.indexOf(op)];
     }
 
-    private boolean isOp (String op) {
-
+    private static boolean isOp (String op) {
+	return operations.contains(op);
     }
 
     public static double eval (String str) {
-
+	int answer;
+	String[] ary = str.split(" ");
+	ArrayDeque<Double> dq = new ArrayDeque<Double>();
+	for (String element : ary) {
+	    if (isOp(element)) {
+		dq.push(apply(element, dq.pop(), dq.pop()));
+	    } else {
+		dq.push(Double.parseDouble(element));
+	    }
+	}
+	return dq.pop();
     }
 
     public static void main (String[] args) {
 
 	Eval dank = new Eval();
+
 	System.out.println(Eval.eval("5 2 +"));
 	// 7.0
 	System.out.println(Eval.eval("5 2 -"));
@@ -25,6 +41,8 @@ public class Eval {
 	// 2.0
 	System.out.println(Eval.eval("5 2 %"));
 	// 1.0
+	System.out.println(Eval.eval("2 500 10 10 * - *"));
+	// 800.0
 	
     }
 

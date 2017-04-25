731a704ad7a656fa1b712355322c604bbfa2f4b9
Ahbab Ashraf <ahbab.ashraf@gmail.com>
2017-04-24 21:05:11 -0400

    Made it array based

--- 10Deque/MyDeque.java
+++ 10Deque/MyDeque.java
@@ -3,16 +3,32 @@ import java.util.*;
 public class MyDeque {
 
     private int size, front, back;
-    private LinkedList<String> list;
+    private String[] list;
 
     public MyDeque () {
-	back = -1;
-	list = new LinkedList<String>();
+	list = new String[10];
     }
 
     private void checkSize () {
 	if (size == 0) {
 	    throw new NoSuchElementException();
+	} else if (size == list.length) {
+	    doubleSize();
+	}
+    }
+
+    private void doubleSize () {
+	String[] temp = new String[2 * list.length];
+	int index = 0;
+	// Fill in all elements after and including the front
+	for (int count = front; count < list.length; index++, count++) {
+	    temp[index] = list[count];
+	}
+	// If the back is before front, it adds the rest of the elements
+	if (back != list.length) {
+	    for (int count = 0; count <= back; index++, count++) {
+		temp[index] = list[count];
+	    }
 	}
     }
 
@@ -24,52 +40,55 @@ public class MyDeque {
 
     public void addFirst (String str) {
 	checkNull(str);
-	list.add(front, str);
-        back++;
+	front--;
+	
+	list[front % list.length] = str;
+	System.out.println("k");
 	size++;
+	checkSize();
 	//System.out.println("" + front + " " +  back + " " +  size);
     }
 
     public void addLast (String str) {
 	checkNull(str);
-	list.add(str);
 	back++;
+	list[back % list.length] = str;
 	size++;
+	checkSize();
 	//System.out.println("" + front + " " + back + " " +  size);
     }
 
     public String removeFirst () {
 	checkSize();
-	String first = getFirst();
+	String first = list[front];
+	front = (front + 1) % list.length;
 	return first;
     }
 
     public String removeLast () {
 	checkSize();
-	String last = getLast();
+	String last = list[back];
+	back = (back - 1) % list.length;
 	return last;
     }
 
     public String getFirst () {
 	checkSize();
 	//System.out.println(front);
-	return list.get(front);
+	return list[front];
     }
 
     public String getLast () {
 	checkSize();
 	//System.out.println(back);
-	return list.get(back);
+	return list[back];
     }
-
-    private void doubleSize () {
-
-    }
-
+    
     public static void main (String[] args) {
 	
 	MyDeque dank = new MyDeque();
 	dank.addFirst("hello");
+	System.out.println("added hello");
 	dank.addFirst("world");
 	System.out.println(dank.getFirst());
 	System.out.println(dank.getLast());

21d91cc1f543731ed2c1363e6e35ef7138cbed8a
Ahbab Ashraf <ahbab.ashraf@gmail.com>
2017-05-03 14:48:10 -0400

    Wrote skeleton for heap

---
+++ 13MyHeap/MyHeap.java
@@ -0,0 +1,42 @@
+public class MyHeap {
+
+    private ArrayList<String> heap;
+    private int constant = 1, size;
+
+    public MyHeap () {
+	size = 0;
+    }
+
+    // True for max-heap, false for min-heap
+    public MyHeap (boolean bool) {
+	if (!bool) {
+	    constant = -1;
+	}
+	
+    }
+
+    public void add (String str) {
+
+    }
+
+    public String remove () {
+	String ans = "";
+	
+	return ans;
+    }
+
+    public String peek () {
+	String ans = "";
+	
+	return ans;
+    }
+
+    private void pushUp (int num) {
+
+    }
+
+    private void pushDown (int num) {
+
+    }
+
+}
